d8d84a3091c71b7a114408f09bc58ec466a829fd
cam292 <camazzotta@sei.cmu.edu>
2019-08-06 16:37:32 -0400

    Added API server and shell of request handlers

---
+++ src/main/java/edu/cmu/sei/kalki/api/APIServerStartup.java
@@ -0,0 +1,26 @@
+package edu.cmu.sei.kalki.api;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class APIServerStartup {
+    private static final String API_URL = "/iot-interface-api";
+    private static final int SERVER_PORT = 9090;
+
+    /**
+     * Starts a Jetty server, with handler for notifications
+     */
+    public static void start() {
+        try {
+            Server httpServer = new Server(SERVER_PORT);
+            ServletContextHandler handler = new ServletContextHandler(httpServer, API_URL);
+            handler.addServlet(NewDeviceServlet.class, "new-device/");
+            handler.addServlet(UpdateDeviceServlet.class, "update-device");
+            handler.addServlet(SendCommandServlet.class, "send-commad/");
+            httpServer.start();
+        } catch (Exception e) {
+            System.out.println("Error starting IoT Interface API Server");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/edu/cmu/sei/kalki/api/UpdateDeviceServlet.java
@@ -0,0 +1,18 @@
+package edu.cmu.sei.kalki.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.http.HttpStatus;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class UpdateDeviceServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
+    }
+}
