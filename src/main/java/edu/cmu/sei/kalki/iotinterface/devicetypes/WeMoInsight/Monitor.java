f73d5d6540fab2a26fcce7f1d5d315f1a1415393
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-01-30 23:26:45 -0500

    Reduced duplicated code that handles wemo.py script

--- src/main/java/edu/cmu/sei/kalki/iotinterface/devicetypes/WeMoInsight/Monitor.java
+++ src/main/java/edu/cmu/sei/kalki/iotinterface/devicetypes/WeMoInsight/Monitor.java
@@ -3,10 +3,6 @@ package edu.cmu.sei.kalki.iotinterface.devicetypes.WeMoInsight;
 import edu.cmu.sei.kalki.iotinterface.devicetypes.PollingMonitor;
 import edu.cmu.sei.ttg.kalki.models.DeviceStatus;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.StringBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,43 +22,17 @@ public class Monitor extends PollingMonitor {
     @Override
     public void pollDevice(DeviceStatus status) {
         try {
+            String output = WemoScript.executeScript("status", this.deviceIp);
 
-            // run the command
-            // using the Runtime exec method:
-            String[] args = new String[]{
-                    "python",
-                    "wemo.py",
-                    deviceIp,
-                    "status"
-            };
-            Process p = Runtime.getRuntime().exec(args);
-
-            BufferedReader stdInput = new BufferedReader(new
-                    InputStreamReader(p.getInputStream()));
-
-            BufferedReader stdError = new BufferedReader(new
-                    InputStreamReader(p.getErrorStream()));
-
-            // read the output from the command
-            StringBuilder st = new StringBuilder();
-            String s = null;
-            while ((s = stdInput.readLine()) != null) {
-                st.append(s);
+            if(output != null) {
+                JSONObject json = new JSONObject(output);
+                for (Object keyObj : json.keySet())
+                {
+                    String key = (String) keyObj;
+                    String value = json.get(key).toString();
+                    status.addAttribute(key, value);
+                }
             }
-	        logger.info("Output from device: "+st.toString());
-            JSONObject json = new JSONObject(st.toString());
-            for(Object keyObj : json.keySet()){
-                String key = (String) keyObj;
-                String value = (String) json.get(key).toString();
-                status.addAttribute(key, value);
-            }
-            // read any errors from the attempted command
-            while ((s = stdError.readLine()) != null) {
-                logger.severe(s);
-            }
-
-        } catch (IOException e) {
-            logger.severe(logId + " Error polling Wemo Insight: " + e.toString());
         } catch (JSONException e) {
             logger.severe(logId + " Error parsing JSON response from Wemo Insight: " + deviceId + ". " + e.getMessage());
 	        e.printStackTrace();
--- src/main/java/edu/cmu/sei/kalki/iotinterface/devicetypes/WeMoInsight/WemoScript.java
+++ src/main/java/edu/cmu/sei/kalki/iotinterface/devicetypes/WeMoInsight/WemoScript.java
@@ -1,5 +1,70 @@
 package edu.cmu.sei.kalki.iotinterface.devicetypes.WeMoInsight;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
 public class WemoScript
 {
+    private static Logger logger = Logger.getLogger("iot-interface");
+    private static final String logId = "[WemoScript] ";
+
+    /**
+     * Configures command line arguments to execute the wemo python script
+     * @param deviceIp The ip of the WemoInsight device
+     * @param command The command to be sent. Options: turn-off, turn-on, status (not applicable here)
+     * @return Array of command line args to execute the script
+     */
+    private static String[] setArgs(String deviceIp, String command){
+        return new String[]{
+                "python",
+                "wemo.py",
+                deviceIp,
+                command
+        };
+    }
+
+    /**
+     * Executes the wemo python script and process output
+     * @param command The string that is the command's name
+     */
+    public static String executeScript(String command, String deviceIp){
+        try {
+            String[] args = setArgs(deviceIp, command);
+            Process p = Runtime.getRuntime().exec(args);
+            logger.info(logId + " Python script executed");
+
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+            logger.info(logId + " Input stream captured");
+
+            BufferedReader stdError = new BufferedReader(new
+                    InputStreamReader(p.getErrorStream()));
+            logger.info(logId + " Error stream captured");
+
+            logger.info(logId + " Processing input stream");
+            String line;
+            StringBuilder fullOutput = new StringBuilder();
+            while ((line = stdInput.readLine()) != null) {
+                logger.info(logId + line);
+                fullOutput.append(line + "\n");
+            }
+
+            logger.info(logId + " Processing error stream");
+            StringBuilder error = new StringBuilder();
+            while ((line = stdError.readLine()) != null) {
+                error.append(line + "\n");
+            }
+            if(error.toString().length()>0) {
+                logger.severe(logId + " Error with wemo script: " + error.toString());
+            }
+
+            return fullOutput.toString();
+        } catch (IOException e) {
+            logger.severe(logId + " Error reading response from device " + deviceIp);
+            logger.severe(e.getMessage());
+            return null;
+        }
+    }
 }
