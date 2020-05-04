07b68959e72a67a8899070b963dcf5b1381f116e
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-04 16:45:05 -0400

    Trying to get docker-compose to use proxy

--- build_compose.sh
+++ build_compose.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 ./gradlew build
-docker-compose build
+docker-compose build --build-arg HTTP_PROXY=http://proxy.sei.cmu.edu:8080 \
+                     --build-arg HTTPS_PROXY=http://proxy.sei.cmu.edu:8080 \
+                     --build-arg http_proxy=http://proxy.sei.cmu.edu:8080 \
+                     --build-arg https_proxy=http://proxy.sei.cmu.edu:8080
