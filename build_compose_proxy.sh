f306a832d2e60689339dbd15b4d08505ac03ddda
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-04 16:52:02 -0400

    Add script to specifically pass proxy

---
+++ build_compose_proxy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#Docker-compose bug requires manually passing proxy vars.
+
+./gradlew build
+docker-compose build --build-arg HTTP_PROXY=${HTTP_PROXY} \
+                     --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+                     --build-arg http_proxy=${http_proxy} \
+                     --build-arg https_proxy=${https_proxy} \
+                     kalki-iot-interface
+
+docker-compose build --build-arg HTTP_PROXY=${HTTP_PROXY} \
+                     --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+                     --build-arg http_proxy=${http_proxy} \
+                     --build-arg https_proxy=${https_proxy} \
+                     kalki-iot-wemo
