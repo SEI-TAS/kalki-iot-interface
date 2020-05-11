1fd32e49c87e2a40c7bf1b854fcb96aae234e895
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-11 15:47:19 -0400

    Fixed path

--- create_dist.sh
+++ create_dist.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+DIST_PATH=$1
+if [ -z "${DIST_PATH}" ]; then
+  echo "Destination dist path argument required"
+  exit 1
+fi
+
 # Needed as workaround to docker-compose bug in production.
-mkdir -p plugins/WeMoInsight/api
+mkdir -p ${DIST_PATH}/plugins/WeMoInsight/api
