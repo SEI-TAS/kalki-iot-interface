caa3c0893057148f29e9931f347b39db43ca6388
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-11 23:43:47 -0400

    Typo

--- build_container.sh
+++ build_container.sh
@@ -2,8 +2,8 @@
 
 # Pass proxy info, if any, to gradle inside the docker first stage.
 IFS=':' read PROXY_HOST PROXY_PORT <<<"$(echo ${http_proxy/http:\/\//})"
-echo -en 'systemProp.http.proxyHost=${PROXY_HOST}\nsystemProp.http.proxyPort=${PROXY_PORT}\n' >> gradle.properties
-echo -en 'systemProp.https.proxyHost=${PROXY_HOST}\nsystemProp.https.proxyPort=${PROXY_PORT}\n' >> gradle.properties
+echo -en "systemProp.http.proxyHost=${PROXY_HOST}\nsystemProp.http.proxyPort=${PROXY_PORT}\n" >> gradle.properties
+echo -en "systemProp.https.proxyHost=${PROXY_HOST}\nsystemProp.https.proxyPort=${PROXY_PORT}\n" >> gradle.properties
 
 docker build -t kalki/kalki-iot-interface .
 
