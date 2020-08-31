afe0e928795a9d604263d7d9f61e71767c244d16
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-08-31 11:03:54 -0400

    Added option to skip tests

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-docker build -t kalki/kalki-iot-interface .
+
+SKIP_TESTS_ARG=""
+if [ "$1" == "--skip_tests" ]; then
+  SKIP_TESTS_ARG=" -x test "
+fi
+
+docker build --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" -t kalki/kalki-iot-interface .
 
 (cd plugins/WeMoInsight/api && bash build_container.sh)
