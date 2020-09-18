35c452e4cda9a4be81b8054d18edef49d809b122
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-09-18 15:47:29 -0400

    Added support for selecting version of kalki-db to use as base container image

--- build_container.sh
+++ build_container.sh
@@ -5,6 +5,7 @@ if [ "$1" == "--skip_tests" ]; then
   SKIP_TESTS_ARG=" -x test "
 fi
 
-docker build --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" -t kalki/kalki-iot-interface .
+KALKI_DB_VER="1.7.0"
+docker build --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" --build-arg KALKI_DB_VER="${KALKI_DB_VER}" -t kalki/kalki-iot-interface .
 
 (cd plugins/WeMoInsight/api && bash build_container.sh)
