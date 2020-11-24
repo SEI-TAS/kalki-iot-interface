3c3942559221e8ec3fd072aa8cc26e07a38f563c
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-11-24 14:17:52 -0500

    Updating kalkidb version

--- build_container.sh
+++ build_container.sh
@@ -5,7 +5,7 @@ if [ "$1" == "--skip_tests" ]; then
   SKIP_TESTS_ARG=" -x test "
 fi
 
-KALKI_DB_VER="1.7.0"
+KALKI_DB_VER="1.8.0"
 docker build --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" --build-arg KALKI_DB_VER="${KALKI_DB_VER}" -t kalki/kalki-iot-interface .
 
 (cd plugins/WeMoInsight/api && bash build_container.sh)
